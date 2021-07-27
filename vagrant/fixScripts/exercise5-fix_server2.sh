b5cc96ca26f81ed4e02161815a9108fc5da1fa42
Aleksandr Korshunov <aleks.corshunov@gmail.com>
2021-07-27 06:26:48 +0300

    done

--- vagrant/fixScripts/exercise5-fix_server2.sh
+++ vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+keyspath="/vagrant/keys"
+
+cp "$keyspath/server2" /home/vagrant/server2_key
+chown vagrant:vagrant /home/vagrant/server2_key
+chmod 600 /home/vagrant/server2_key
+
+cat "$keyspath/server1.pub" >> /home/vagrant/.ssh/authorized_keys
+
+cp "$keyspath/server2" /root/server2_key
+chown root:root /root/server2_key
+chmod 600 /root/server2_key
+
+cat "$keyspath/server1.pub" >> /root/.ssh/authorized_keys
+
+sed -i '/Include/a Host server1 192.168.100.10\nStrictHostKeyChecking no\nPasswordAuthentication no\nIdentityFile ~/server2_key\nIdentitiesOnly yes' /etc/ssh/ssh_config
+service ssh restart
+
+rm -rf "$keyspath"
